d2fcb9222b748f4c9a0bdb3c91faa0bb26c26953
Leonardo <leonardoguitar2010@gmail.com>
2017-03-15 00:33:40 +0000

    matrix_histogram.m created online with Bitbucket

--- code/matrix_histogram.m
+++ code/matrix_histogram.m
@@ -1,87 +1,72 @@
-function M = matrix_histogram(samples,angles,nM,b,m,method) 
+function M = matrix_histogram(samples, option) 
+% M = MATRIX_HISTOGRAM(samples, option) 
+%  This functions makes a matrix histogram using as input samples a matrix
+%  Mx3 of columns [angles, measurements of quadratures, measures number in each bin]
+%  and a option using this pattern:
+%   option = 
+%       1 = 'auto'
+%       2 = 'scott'
+%       3 = 'fd'
+%       4 = 'integers'
+%       5 = 'sturges'
+%       6 = 'sqrt'
+%       values grater than 6 indicates a number of bins.
 
-% matrix_histogram constructs an array from samples whose columns are respectively
-% . m equally spaced angles between 0 to pi with each angle being repeated a thousand times
-% . Center of the boxes of each histogram formed by the quadrature measurements of samples of each angle separately, placed in "b" bins.
-% . Number of quadrature measure counts in each bin in respective histograms
+num_measurements = size(samples, 1);
+num_angles = num_measurements/1000;
+method = {'auto', 'scott', 'fd', 'integers', 'sturges', 'sqrt'};
 
-%Na matriz H, cada coluna é igual aos valores das medidas em quadratura da matriz de amostras para cada ângulo separadamente.
-% In matrix H, each column equals the values of the quadrature measurements of the samples matrix for each angle separately.
-H = zeros(nM/m,m);
+% Specify number of bins
+if option > 6,
+    num_bins = option;
+    angles = pi*(0:num_angles-1)/num_angles;
+    angles = repmat(angles,b,(num_measurements/num_angles));
+    angles = angles(1:num_bins*num_angles)';
+    A = angles;
 
-%Na matriz A, cada ângulo é repetido "b" vezes em cada coluna para transformá-la em um vetorcoluna para a construção de M.
-% In matrix A, each angle is repeated a thousand times in each column in order to transform it into a column vector for the construction of M.
-
-% angles = pi*[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,19]/m;
-% angles = repmat(angles,N(:,i),(nM/m));
-%                 angles = angles(1:N(:m)';
-%             A = angles;
-
-            
-%Na matriz N, temos o número de contagem das caixas dos histogramas de cada ângulo igualmente espaçado.
-% In matrix N, we have the count number of the bins of the histograms of each equally spaced angle.
-
-N = zeros(b,m);
-
- 
-%Na matriz C, cada coluna é igual aos centros das caixas dos histogramas de cada ângulo separadamente, ambos colocados em 1000 caixas.
-% In matrix C, each column equals the centers of the boxes of the histograms of each angle separately, both placed in 1000 bins.
-% C = zeros(b,m);
-
-% C = zeros(N(i),m);
-M = zeros(1,3);
+    H = zeros(num_measurements/num_angles, num_angles);
+    N = zeros(num_bins, num_angles);
 
     for i=1:m;
          
-        angle = samples(i,1); 
-        
         H(:,i) = samples((i:m:end),2); 
-        
-%        [N(:,i),edges] = histcounts(H(:,i),b);
-
-  [N,edges] = histcounts(H(:,i),'BinMethod',method);
-  
-       d = diff(edges)/2;
-       
-  
-       centers = edges(1:end-1)+d;
-       
-        C = centers';
-        
-            
-        MA = [repmat(angle,length(N),1), C, N'];
-        
-        M = [M; MA];
-        
-%         figure;
-% 
-%         title('h(i)');
-%      
-%         h(i) = histogram(H(:,i),b)
-        
-        
+        [N(:,i),edges] = histcounts(H(:,i), num_bins);
+        d = diff(edges)/2;
+        centers = edges(1:end-1)+d;
+        C = centers';        
     end
     
-    M =M(2:end,:);
+    A2 = A(:);
+    C2 = C(:);
+    N2 = N(:);
     
-    % A2, C2 e N2 são as matrizes A,C e N transformadas em vetores
-    % A2, C2 and N2 are the matrices A, C and N transformed into vectors
-%     A2 = A(:);
-%     C2 = C(:);
-%     N2 = N(:);
-     
-    % A função "ind" seleciona os índices de N2 cujo valor é maior que zero
-    % The "ind" function selects the indices of N2 whose value is greater than zero
-%     ind = find(N2 > 0);
-%     
-    % A3, C3, e N3 são os vetores com os valores de A,C e N respectivamente
-    % cujo os índices de N são maiores que zero
+    ind = find(N2 > 0);
     
-    % A3, C3, and N3 are the vectors with the values of A, C and N respectively
-    % Whose N indices are greater than zero
+    A3 = A2(ind);
+    C3 = C2(ind);
+    N3 = N2(ind);
     
-%     A3 = A2(ind);
-%     C3 = C2(ind);
-%     N3 = N2(ind);
-%     
-%     M = [A3, C3, N3];
+    M = [A3, C3, N3];
+else
+    % Specify method
+    if (option > 0) && (option < 7),
+
+        H = zeros(num_measurements/num_angles, num_angles);
+        M = zeros(1, 3);
+
+        for i=1:num_angles;
+
+            angle = samples(i,1);
+            H(:,i) = samples((i:num_angles:end),2); 
+            [N,edges] = histcounts(H(:,i),'BinMethod', method{option});
+            d = diff(edges)/2;
+            centers = edges(1:end-1)+d;
+            C = centers';
+            MA = [repmat(angle,length(N),1), C, N'];
+            M = [M; MA];
+
+        end
+
+        M =M(2:end,:);
+    end
+end
