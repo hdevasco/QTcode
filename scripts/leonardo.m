64ae7451c1c274225488c75a69306c19bd574b17
Leonardo <leonardoguitar2010@gmail.com>
2017-03-17 17:02:02 +0000

    leonardo.m created online with Bitbucket

--- scripts/leonardo.m
+++ scripts/leonardo.m
@@ -35,74 +35,28 @@ num_measurements = [20000];
 
 
 % Simulates the reconstruction of the quantum state using the bins number method determined for the histogram
-
-% num_bins = [100];
-
-% for i=1:length(Mph),
-%     for j=1:length(num_bins),
-%         for k=1:length(num_measurements),
-%             
-%             fileName = ['maxnumberMph',num2str(Mph(i)),'b',num2str(num_bins(j)),'nM',num2str(num_measurements(k)),'.mat'];
-
-%  if exist(fileName,'file') == 2,
-%                 load(fileName);
-%             else
-%                 maxPhotonNumber = Mph(i);
-%                 num_bins = num_bins(j);
-%                 num_sim  = 100;
-%                 F1 = zeros(num_sim,1);
-%                 F2 = zeros(num_sim,1);
-%                 F3 = zeros(num_sim,1);
-%                 F4 = zeros(num_sim,1);
-%                 W = zeros(num_sim,1);
-%                 T1 = zeros(num_sim,1);
-%                 T2 = zeros(num_sim,1);
-%                 T3 = zeros(num_sim,1);
-%                 T4 = zeros(num_sim,1);
-%                 T5= zeros(num_sim,1);
-%                
-%                 num_measurements       = num_measurements(k);
-%                 etaDetector         = 0.9;
-%                 maxIterations       = 2000;
-%                 stoppingCriterion   = 0.01;
-%                 alpha = 1;
-%                 phase = 0;
-%                 etaState = 0.8;
-%                 t=1;
-%                 
-%                 save(fileName);
-%             end
-%             while t <= num_sim,
-
-% Simula a reconstrução do estado usando o método de largura ótima para o
-% histograma
-
-% Simulates quantum state reconstruction using the optimal width method for the histogram
-
-option = [2];
+num_bins = [100];
 
 for i=1:length(Mph),
-    for j=1:length(option),
+    for j=1:length(num_bins),
         for k=1:length(num_measurements),
             
-            fileName = ['maxnumberMph',num2str(Mph(i)),'option',num2str(option(j)),'nM',num2str(num_measurements(k)),'.mat'];
-            
-            if exist(fileName,'file') == 2,
+            fileName = ['maxnumberMph',num2str(Mph(i)),'b',num2str(num_bins(j)),'nM',num2str(num_measurements(k)),'.mat'];
+
+ if exist(fileName,'file') == 2,
                 load(fileName);
             else
                 maxPhotonNumber = Mph(i);
-                num_bins = option(j);
+                num_bins = num_bins(j);
                 num_sim  = 100;
-                F1 = zeros(num_sim,1);
-                F2 = zeros(num_sim,1);
-                F3 = zeros(num_sim,1);
-                F4 = zeros(num_sim,1);
-                W = zeros(num_sim,1);
-                T1 = zeros(num_sim,1);
-                T2 = zeros(num_sim,1);
-                T3 = zeros(num_sim,1);
-                T4 = zeros(num_sim,1);
-                T5= zeros(num_sim,1);
+                FML2 = zeros(num_sim,1);
+                Fhistogram = zeros(num_sim,1);
+                Fhistogrampsi = zeros(num_sim,1);
+                FML2psi = zeros(num_sim,1);
+                Whistogram = zeros(num_sim,1);
+                TimeML2 = zeros(num_sim,1);
+                TimeMhistogram = zeros(num_sim,1);
+                Timerhohistogram = zeros(num_sim,1);
                
                 num_measurements       = num_measurements(k);
                 etaDetector         = 0.9;
@@ -116,8 +70,8 @@ for i=1:length(Mph),
                 save(fileName);
             end
             while t <= num_sim,
-                
-                fprintf('>> Generating angles... ');
+
+   fprintf('>> Generating angles... ');
                 tic;
                 
                 num_angles = 20;% Number of equally spaced angles
@@ -135,140 +89,148 @@ for i=1:length(Mph),
                 disp('>> apply loss...');
                 rho = apply_loss(psi,etaState,S);
                 
-                
-                % Agora, fazemos as medições do estado rho. Observe que temos que especificar
-                % Resultados de medição máximo e mínimo possíveis -7 e 7.
-                
-                % Now, we make the measurements of state rho.  Notice we have to specify
-                % maximum and minimum possible measurement results -7 and 7.
-                
+             
                 fprintf(['>> homodyne samples... iteraction ',num2str(t), '\n']);
                 samples = homodyne_samples(-7,7,etaDetector,angles,rho,S);
-                
-                % Estrutura contendo o elemento POVM correspondente a cada medida
-                % resultado. Observe que os POVMs nÃ£o sÃ£o projetores puros.
-                % A eficiência do detector foi incluída no cálculo dos POVMs.
-                
-                % Structure containing the POVM element corresponding to each measurement
-                % result.  Note that the POVMs are not pure projectors.  The homodyne
-                % detector's efficiency has been included in the computation of the POVMs.
-                %  tic;
-                %     Povms = make_measurement_struct(samples,etaDetector,S);
-                
-                % Agora vamos usar o algoritmo R * rho * R até termos feito 2000 iterações
-                % Ou chegarmos a parar a diferença 0.01 (o que ocorrer primeiro).
-                % A intercepção é um limite superior da diferença entre o verdadeiro
-                % Log-verossimilhanÃ§a máxima e a log-verossimilhança do estado de iterações.
-                
-                % Now we will use the R*rho*R algorithm until we have done 2000 iterations
-                % or we reach stoppingCriterion 0.01 (whichever happens first).
-                % stoppingCriterion is an upper bound on the difference between the true
-                % maximum log-likelihood and the log-likelihood of that iterations's state.
-                
-                
-                
-                
-                %  [rhoML1, Diagnostics] = rrhor_optimization(samples, S, etaDetector, 0, maxIterations, stoppingCriterion, []);
-                %  Saída é o estado de mÃ¡xima verossimilhança, rhoML e uma estrutura grande
-                %   de diagnósticos contendo informações sobre o progresso de cada iteração.
-                %
-                %   Em vez de usar R * rho * R, podemos usar uma combinaÃ§Ã£o de R * rho * R seguido
-                %   Por iterações de nosso novo algoritmo, a ascensão gradiente regularizada.
-                %   Isso geralmente é mais rápido, especialmente se você quiser estar no
-                %   Verdadeiro estado de máxima verossimilhança.
-                
-                % output is the maximum likelihood state, rhoML, and a big structure
-                % Diagnostics containing information about each iterations's progress.
-                
-                % Rather than using R*rho*R, we can use a combination of R*rho*R followed
-                % by iterations of our new algorithm, the regularized gradient ascent.
-                % This is usually faster, especially if you want to be very close to the
-                % true maximum likelihood state.
+               
+
                 fprintf(['>> combined optimization... iteraction ',num2str(t), '\n']);
                 tic;
                 [rhoML2, Diagnostics] = combined_optimization( samples, S, etaDetector, 0, maxIterations, stoppingCriterion);
                 
-                T1(t) = toc;
+                TimeML2(t) = toc;
+                
                 % Fidelidade entre o estado verdadeiro e o estado estimado.
                 
                 % Fidelity of true state and estimate
-%                 disp('>> fidelity (samples)...');
-                tic;
-                F1(t) = fidelity(rhoML2, rho);
-                T2(t)= toc;
                 
-               
+                disp('>> fidelity (rhoML2 x rho)...');
                 
-                %Constrói a matriz M que tem como linhas(Ângulo, centro do bin,número de contagens no bin)
+                FML2(t) = fidelity(rhoML2, rho);
                 
-                % It constructs the matrix M that has as lines (angle, center of the bin, number of counts in the bin)
                 
                 disp('>> matrix histogram ...');
                 
-%                 M  será construída a partir de histogramas utilizando as medidas
-%                 de quadratura de samples nas 20 fases igualmente espaçadas de 0 a pi.
-%                 Usaremos o método de Scott para estimar a largura ótima dos bins.
-                
-                % M will be constructed from histograms using the measures
-                % of quadrature of samples in the 20 equally spaced phases from 0 to pi. 
-                % We will use Scott's method to estimate the optimal width of the bins.
-                
-                option = 2 ;
-                
+                option = [100] ;
+               
                 tic;
                 
-                M = matrix_histogram(samples,option);
-                T3(t) = toc;
+                Mhistogram = matrix_histogram(samples,option);
+                TimeMhistogram(t) = toc;
               
                
-                %     Povmshistogram = make_measurement_struct(M,etaDetector,S);
-                
-                % [rhoML1, Diagnostics] = rrhor_optimization(M, S, etaDetector, 0, maxIterations, stoppingCriterion, []);
-                
-%                 fprintf(['>> combined optimization (M)... iteraction ',num2str(t), '\n']);
                 tic;
-                [rhohistogram, Diagnostics] = combined_optimization( M, S, etaDetector, 0, maxIterations, stoppingCriterion);
-                T4(t) = toc;
-                
-                % Calcula a fidelidade entre o estado verdadeiro e o estado reconstruído
-                % usando o histograma
-                
-                % Calculates the fidelity between the true state and the reconstructed state using the histogram
-%                 disp('>> fidelity (rhohistogram x rho)...');
-                 tic;
+                [rhohistogram, Diagnostics] = combined_optimization( Mhistogram, S, etaDetector, 0, maxIterations, stoppingCriterion);
+                Timerhohistogram(t) = toc;
+             
 
-                F2(t) = fidelity(rhohistogram, rho);
-                 T5(t)= toc;
+                Fhistogram(t) = fidelity(rhohistogram, rho);
+                 
                 
                 disp('>> fidelity (rhohistogram x psi)...');
                  
-                  F3(t) = fidelity(rhohistogram, psi);
+                  Fhistogrampsi(t) = fidelity(rhohistogram, psi);
                 
                 
                 disp('>> fidelity (rhoML2 x psi)...');
                 
-                F4(t) = fidelity(rhoML2,psi);
-               
+                FML2psi(t) = fidelity(rhoML2,psi);
                 
-                W(t)= F1(t)-F2(t);
-                w= mean(W);
-                D = std(W);
-                f1 = mean (F1); % Average fidelity (rhoML2,rho)
-                f2 = mean (F2); % Average fidelity (rhohistogram, rho)
-                f3 = mean (F3); % Average fidelity (rhohistogram, psi)
-                f4 = mean (F4); % Average fidelity (rhoML2, psi)
-                t1 = mean (T1); % Average time to build (rhoML2)
-                t2 = mean (T2); % Average time to calculate  fidelity (rhoML2,rho)
-                t3 = mean (T3); % Average time to build M
-                t4 = mean (T4); %  Average time to build (rhohistogram)
-                t5 = mean (T5); % Average time to calculate fidelity(rhohistogram, rho)
-                d1 = std(T1); %  Standard deviation of the mean time of the rhoML2 construct
-                d2 = std(T2); % Standard deviation of the fidelity estimation time (rhoML2, rho)
-                d3 = std(T3); %  Standard deviation of the mean time of construction of M
-                d4 = std(T4); % Standard deviation of the mean time of the rhohistogram construction
-                d5 = std(T5); % Standard deviation of the fidelity estimation time (rhohistogram, rho)
-                dF1= std(F1); % Standard deviation of the  fidelity of (rhoML2, rho)
-                dF2= std(F2); % Standard deviation of the  fidelity of (rhohistogram, rho)
+                Whistogram(t)= FML2(t)-Fhistogram(t);
+                whistogram= mean(Whistogram);
+                Dhistogram = std(Whistogram);
+                fML2 = mean(FML2); 
+                fhistogram = mean(Fhistogram);
+                fML2psi = mean(FML2psi);
+                fhistogrampsi = mean(Fhistogrampsi);
+                timeML2 = mean(TimeML2); 
+                timeMhistogram = mean(TimeMhistogram); 
+                timerhohistogram = mean(Timerhohistogram);
+                dFhistogram= std(Fhistogram); 
+                
+                
+                home;
+                fprintf('>> Progress: %.2f%%\n', t/num_sim*100);
+                t=t+1;
+                
+                save(fileName);
+            end
+        end
+    end
+end             
+                
+% Simula a reconstrução do estado usando o método de largura ótima para o
+% histograma
+% 
+% Simulates quantum state reconstruction using the optimal width method for the histogram
+
+option = [2];
+
+for i=1:length(Mph),
+    for j=1:length(option),
+        for k=1:length(num_measurements),
+            
+            fileName = ['maxnumberMph',num2str(Mph(i)),'option',num2str(option(j)),'nM',num2str(num_measurements(k)),'.mat'];
+            
+            if exist(fileName,'file') == 2,
+                load(fileName);
+            else
+                maxPhotonNumber = Mph(i);
+                num_bins = option(j);
+                num_sim  = 100;
+                Fscott = zeros(num_sim,1);
+                Fscottpsi = zeros(num_sim,1);
+                TimeMscott = zeros(num_sim,1);
+                Timerhoscott= zeros(num_sim,1);
+                Wscott = zeros(num_sim,1);
+                
+                
+                num_measurements       = num_measurements(k);
+                etaDetector         = 0.9;
+                maxIterations       = 2000;
+                stoppingCriterion   = 0.01;
+                alpha = 1;
+                phase = 0;
+                etaState = 0.8;
+                t=1;
+                
+                save(fileName);
+            end
+            while t <= num_sim,
+                
+               
+                disp('>> matrix scott ...');
+                
+
+                option = 2 ;
+                tic;
+                
+                Mscott = matrix_histogram(samples,option);
+                TimeMscott(t) = toc;
+              
+               
+                tic;
+                [rhoscott, Diagnostics] = combined_optimization( Mscott, S, etaDetector, 0, maxIterations, stoppingCriterion);
+                
+                Timerhoscott(t) = toc;
+              
+
+                Fscott(t) = fidelity(rhoscott, rho);
+                 
+                
+                disp('>> fidelity (rhoscott x psi)...');
+                 
+                  Fscottpsi(t) = fidelity(rhoscott, psi);
+    
+                
+                Wscott(t)= FML2(t)-Fscott(t);
+                wscott= mean(Wscott);
+                Dscott = std(Wscott);
+                fscott = mean(Fscott); 
+                fscottpsi = mean(Fscottpsi); 
+                timeMscott = mean(TimeMscott); 
+                timerhoscott = mean(Timerhoscott); 
+                dFscott= std(Fscott); 
                 home;
                 fprintf('>> Progress: %.2f%%\n', t/num_sim*100);
                 t=t+1;
@@ -278,4 +240,3 @@ for i=1:length(Mph),
         end
     end
 end
-toc;
