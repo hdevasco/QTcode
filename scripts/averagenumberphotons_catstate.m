1d9a218403dc97d962ae4d67b960274ca3a2fc34
Leonardo <leonardoguitar2010@gmail.com>
2017-06-13 15:11:54 +0000

    averagenumberphotons_catstate.m created online with Bitbucket

--- scripts/averagenumberphotons_catstate.m
+++ scripts/averagenumberphotons_catstate.m
@@ -1,4 +1,4 @@
-% Average number of protons according to the alpha value (catstate)
+% (<Psi|n|Psi>) Average number of protons according to the alpha value (catstate)
 
 average_number_photons = zeros(9,1);
 
@@ -6,7 +6,7 @@ alpha = [0,1, 2, 3, 4, 5, 6, 7, 8];
 
 for j=1:1:9;
 
-psi = generate_cat_vector(j-1, 0, 10)
+psi = generate_cat_vector(j-1, 0, 100)
 
 rho = psi*psi';
 
