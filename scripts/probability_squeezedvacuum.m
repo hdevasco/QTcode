efecfb40b80a933478ef0aa50a4b8a6a26d06838
Leonardo <leonardoguitar2010@gmail.com>
2017-06-20 12:42:54 +0000

    probability_squeezedvacuum.m created online with Bitbucket

--- scripts/probability_squeezedvacuum.m
+++ scripts/probability_squeezedvacuum.m
@@ -14,16 +14,17 @@ theta = 0;
 
 for i= 1:9; 
 
- z = ((i-1)+(i-1)*exp(1j*theta)*tanh(r))/((2*exp(1j*theta)*tanh(r))^(-1/2));
+ z = ((i-1)+(i-1)*exp(1j*theta)*tanh(r))/((2*exp(1j*theta)*tanh(r))^(1/2));
  
  
  t = (-((i-1)^2)-(1/2)*(((i-1)^2)*exp(1j*theta)+((i-1)^2)*exp(-1j*theta))*tanh(r));
  
  
- P(i)= (((1/2)*(tanh(r))^n)/((factorial(n)*cosh(r)))*exp(t)*abs(hermiteH(10,z))^2);
+ P(i)= (((1/2)*(tanh(r)))^n)/((factorial(n)*cosh(r)))*exp(t)*abs(hermiteH(10,z))^2;
  
  
 end
 
   plot(alpha,P);       
                 
+
