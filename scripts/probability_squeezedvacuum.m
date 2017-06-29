683efcfa320256804039a2dc52b95423f2eb4799
Leonardo <leonardoguitar2010@gmail.com>
2017-06-29 11:35:04 +0000

    probability_squeezedvacuum.m created online with Bitbucket

--- scripts/probability_squeezedvacuum.m
+++ scripts/probability_squeezedvacuum.m
@@ -1,29 +1,27 @@
-tic;
-clc;
-clear;
+%  Probability of having more than "n" photons in the vacuum state squeezed
 
-% Probability distribution of n photons in squeezed state
+% generate state squeezed
+psi = generate_squeezed_vacuum_vector(3/4, 100, 'ratio');
 
-r = 0.14;
 
-n = 10
+rho = psi*psi';
+
+w = length(psi);
+p_n = zeros(w,1);
+
+for i=1:w-1,
+    n = zeros(w,1);
+    n(i,1) = 1;
     
-alpha = 0:1:8;
-
-theta = 0;
-
-for i= 1:9; 
-
- z = ((i-1)*cosh(r)+(i-1)*exp(1j*theta)*sinh(r))/((exp(1j*theta)*sinh(2*r))^(1/2));
- 
- 
- t = (-((i-1)^2)-(1/2)*(((i-1)^2)*exp(1j*theta)+((i-1)^2)*exp(-1j*theta))*tanh(r));
- 
- 
- P(i)= (((1/2)*(tanh(r)))^n)/((factorial(n)*cosh(r)))*exp(t)*abs(hermiteH(10,z))^2;
- 
- 
+    p_n(i,1) = n'*rho*n;
 end
 
-  plot(alpha,P);       
-                
+% Probability of the squeezed state to have 10 our more photons 
+P_10= 1-sum(p_n(1:11));
+
+
+% Average number of photons
+
+average_number_photons = mean_photons(rho);
+
+
