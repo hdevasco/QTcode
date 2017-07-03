3971442e9b390d6abdf287e46d70a7551c22a3ab
Leonardo <leonardoguitar2010@gmail.com>
2017-07-03 12:54:53 +0000

    probability_vs_alpha_catstate.m created online with Bitbucket

--- scripts/probability_vs_alpha_catstate.m
+++ scripts/probability_vs_alpha_catstate.m
@@ -1,14 +1,20 @@
 % Probability of having 10 photons in the Hilbert space as we vary the number of photons in the cat state
 
-P_10 = zeros(9,1);
+P_10 =                        zeros(9,1);
+average_numberphotons =       zeros(9,1);
 
-alpha = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+alpha =      [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+etaState           =                 0.8;
+
+maxPhotonNumber =                    100;
+S         = init_tables(maxPhotonNumber);
 
 for j= 1:1:9;
 
 psi = generate_cat_vector(j-1, 0, 100)
 
-rho = psi*psi';
+rho = apply_loss(psi,etaState,S);
 
 w = length(psi);
 p_n = zeros(w,1);
@@ -17,12 +23,17 @@ for i=1:w-1,
     n = zeros(w,1);
     n(i,1) = 1;
     
-    p_n(i,1) = n'*rho*n;
+    p_n = abs(psi).^2;
+    
 end
 
-% Probability of obtaining 10 photons
+p_photons = diag(rho);
+% Probability of obtaining 10 our more photons
 
 P_10(j)= 1-sum(p_n(1:11));
+
+
+average_numberphotons(j)= mean_photons(rho);
 
 end
 
