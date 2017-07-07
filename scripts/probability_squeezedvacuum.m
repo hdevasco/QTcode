5f52ed189cc298533182d184a8c477775a2fc9cc
Leonardo <leonardoguitar2010@gmail.com>
2017-07-07 23:14:00 +0000

    probability_squeezedvacuum.m created online with Bitbucket

--- scripts/probability_squeezedvacuum.m
+++ scripts/probability_squeezedvacuum.m
@@ -1,4 +1,3 @@
-
 %  Probability of having more than "n" photons in the vacuum state squeezed
 
 % generate state squeezed
@@ -7,21 +6,18 @@ psi = generate_squeezed_vacuum_vector(3/4, 100, 'ratio');
 maxPhotonNumber = 100;
 S               = init_tables(maxPhotonNumber);
 
-rho = apply_loss(psi, 1/2, S);
+% Apply_loss returns the density matrix after passing through a lossy medium
+rho = apply_loss(psi, 0.8, S);
 
-w = length(psi);
+w = length(rho);
 p_n = zeros(w,1);
 
 
 for i=1:w-1,
-    n = zeros(w,1);
-    n(i,1) = 1;
-    p_n = abs(psi).^2;
-
+    % p_n constructs an array whose lines are the probabilities of p (n).
+    p_n(:,1) = diag(rho);
+    
 end
-
-%Probability of photons
-p_photons = diag(rho);
 
 % Probability of the squeezed state to have 10 our more photons 
 P_10= 1-sum(p_n(1:11));
