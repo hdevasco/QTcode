7ab5814d731c80a8ceed35436a55bb714b2c3dc4
Leonardo <leonardoguitar2010@gmail.com>
2017-08-24 13:46:12 +0000

    deviation_n_rho.m created online with Bitbucket

--- code/deviation_n_rho.m
+++ code/deviation_n_rho.m
@@ -1,18 +1,25 @@
-function sigma = deviation_n(rho)
-%Deviation_n returns the standard deviation associated with the mean number 
+function sigma = deviation_n_rho(rho)
+%Deviation_n_rho returns the standard deviation associated with the mean number 
 %of photons of the rho state, and uses the rho matrix as input.
 
-n   = 0:maxPhotonNumber;
+maxPhotonNumber = size(rho,1)-1;
 
-    % p_n constructs the probabilities of p (n).
+if isvector(rho)
+    %probabilities will be a column vector of photon number probabilities
+    p_n = rho.*conj(rho);
+else
     p_n = diag(rho);
-    
-    
- % mean number of photons   
-average_numberphotons= mean_photons(rho);
+end
 
+
+ n   = 0:maxPhotonNumber;
+ 
+ %  mean number of photons   
+ average_numberphotons= mean_photons(rho);
+ 
 % Variace associated with the mean number of photons
 variance =(n-average_numberphotons).^2 *p_n;
-
+% 
 % Standard deviation associated with the mean number of photons
-sigma = sqrt(variance);
+ sigma = sqrt(variance);
+
