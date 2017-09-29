d53167ff89d744e8a0811aa4f4b6c5c0d1ccac63
Leonardo <leonardoguitar2010@gmail.com>
2017-09-29 16:28:37 +0000

    coarse_measurement.m deleted online with Bitbucket

--- code/coarse_measurement.m
+++
@@ -1,9 +0,0 @@
-function [H_int] = coarse_measurement(paramList,eta, S, returnMatrix)
-% coarse_measurement returns the integrated measurement matrix along the bin where the integration
-% limits x = a and x = b are the edges of the bin.
-
-theta = paramList(1);
-a = paramList(2);
-b = paramList(3);
-H_int = integral(@(x) homodyne_loss_measurement([x,theta],eta,S,returnMatrix),a,b,'ArrayValued',true);
-
