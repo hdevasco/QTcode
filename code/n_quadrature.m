09db6c49bfc3a6deec1d725d3983c2c1d582ed87
Leonardo <leonardoguitar2010@gmail.com>
2017-12-01 17:11:18 +0000

    n_quadrature.m created online with Bitbucket

--- code/n_quadrature.m
+++ code/n_quadrature.m
@@ -2,6 +2,6 @@ function n_bar = n_quadrature(Samples,numMeasurements)
 % n_quadrature estimates the average number of photons from the quadrature measurements.
 x = Samples(:,2);
 
-n_bar = 1/numMeasurements*(sum(x.^2-1/2));
+n_bar = 1/numMeasurements*(sum(x.^2))-1/2;
 
 end
