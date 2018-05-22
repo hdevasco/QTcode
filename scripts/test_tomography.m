0fcb9ac4b855857ae2f78975dcb4c60f395726ab
Leonardo <leonardoguitar2010@gmail.com>
2018-05-22 16:06:46 +0000

    test_tomography.m created online with Bitbucket

--- scripts/test_tomography.m
+++ scripts/test_tomography.m
@@ -1,72 +1,50 @@
 tic;
 clc;
 clear;
-%Maximum number of photons
 
-maxPhotonNumber = [10];
+% savefile = 'raw_quadratures.mat';
+
+% % Maximum number of photons
+% maxPhotonNumber = [10];
 
 % Number of measurements
-numMeasurements    = 20000;
+% numMeasurements    = 20000;
+% 
+% etaDetector        = 0.9;
+% maxIterations      = 2000;
+% stoppingCriterion  = 0.01;
+% alpha              = [1] ;
+% phase              = 0;
+% etaState           = 0.95;
 
-option = [100];
+% % Number of angles equally spaced from 0 to pi
+% numAngles          = 20;
+% 
+% angles = pi*(0:numAngles-1)/numAngles;
+% angles = repmat(angles,1, ceil(numMeasurements/numAngles));
+% angles = angles(1:numMeasurements)';
+% 
+% % Generate state
+% S   = init_tables(maxPhotonNumber);
+% psi = generate_cat_vector(alpha, phase, S);
+% Rho = apply_loss(psi,etaState,S);
 
-%deltaq = [0.5];
+% Samples = homodyne_samples(-7,7,etaDetector,angles,Rho,S);
 
-% Number of simulations
-numSim             = 1;
+% save('raw_quadratures.mat', 'Samples')
 
-etaDetector        = 0.9;
-maxIterations      = 2000;
-stoppingCriterion  = 0.01;
-alpha              = [1] ;
-phase              = 0;
-etaState           = 0.95;
+load('raw_quadratures.mat')
 
-% Number of angles equally spaced from 0 to pi
 numAngles          = 20;
 
+optList = [100];
 
-for i=1:length(maxPhotonNumber),
-    for k=1:length(alpha),
-        for j=1:length(option),
-            
-            fileName1 = ['maxPhNum',num2str(maxPhotonNumber(i)),'option',num2str(option(j)),'alpha',num2str(alpha(k)),'.mat'];
-            
-            if exist(fileName1,'file') == 2,
-                load(fileName1);
-            else
-                t                   = 1;
-                
-                save(fileName1);
-            end
-            
-            while t <= numSim,
-                fprintf(['Iteraction ',num2str(t), '\n']);
-                
-                angles = pi*(0:numAngles-1)/numAngles;
-                angles = repmat(angles,1, ceil(numMeasurements/numAngles));
-                angles = angles(1:numMeasurements)';
-                
-                %generate state
-                
-                S   = init_tables(maxPhotonNumber(i));
-                psi = generate_cat_vector(alpha(k), phase, S);
-                Rho = apply_loss(psi,etaState,S);
-                
-                Samples = homodyne_samples(-7,7,etaDetector,angles,Rho,S);
-                
-                MHistogram_old = matrix_histogram(numAngles, Samples, option(j),'center');
-                
-                MHistogram_new = matrix_histogram_new(numAngles, Samples, option(j),'center');
-                
-                isequal(MHistogram_old, MHistogram_new)
-       
-                home;
-                fprintf('>> Progress: %.2f%%\n', t/numSim*100);
-                t=t+1;
-                
-                save(fileName1);
-            end
-        end
-    end
+for j = 1:length(optList)
+    MHistogram_old_center = matrix_histogram(numAngles, Samples, optList(j),'center');
+    MHistogram_new_center = matrix_histogram_new(numAngles, Samples, optList(j),'center');
+    isequal(MHistogram_old_center, MHistogram_new_center)
+    
+    MHistogram_old_int = matrix_histogram(numAngles, Samples, optList(j),'integral');
+    MHistogram_new_int = matrix_histogram_new(numAngles, Samples, optList(j),'integral');
+    isequal(MHistogram_old_int, MHistogram_new_int)
 end
