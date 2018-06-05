ccb14bae0fc5b31f74b1769aa506416bf46afac9
Leonardo <leonardoguitar2010@gmail.com>
2018-06-05 23:33:20 +0000

    matrix_histogram_new.m created online with Bitbucket

--- code/matrix_histogram_new.m
+++ code/matrix_histogram_new.m
@@ -23,44 +23,34 @@ for i=1:numAngles
     end
 end
 
-if H_operator == 'center'
-        
+if strcmp(H_operator,'center'),
+    
     for i = 1:numAngles;
-        M2 = zeros(1,3);
         d = diff(QuadHist(i).edges)/2;
         QuadHist(i).centers = QuadHist(i).edges(1:end-1)+d;
-       
-    end
-    
-        if option > 8,
         
-            C  = QuadHist(i).centers';
-            A  = angles(i);
-            N  = QuadHist(i).counts;
-            A2 = A(:);
-            C2 = C(:);
-            N2 = N(:);
-            M2 = [A2, C2, N2];
-        else
-            MA = [repmat(angles(i),length(QuadHist(i).counts),1), QuadHist(i).centers', QuadHist(i).counts'];
-            M2 = [M2; MA];
-        end
-            M   = M2(2:end,:);
-            ind = find(M(:,3) > 0);
-            M   = M(ind,:);     
+    end
+    if option > 8
+     
+        QuadHist = [QuadHist(i).angle, QuadHist(i).centers', QuadHist(i).counts'];
+        
+        QuadHist  =QuadHist(2:end,:);
+        
+        ind = find(QuadHist(:,3) > 0);
+        
+        QuadHist = QuadHist(ind,:);
+    end
+end
+if strcmp(H_operator,'integral')
+    for i = 1:numAngles;
+        QuadHist(i).edgesArray = zeros(length(QuadHist(i).edges-1),2);
+        QuadHist(i).edgesArray= [QuadHist(i).edges(1:end-1)',QuadHist(i).edges(2:end)'];
+        QuadHist = [QuadHist(i).angle, QuadHist(i).edgesArray , QuadHist(i).counts];
     end
     
-if H_operator == 'integral'
-      for i = 1:numAngles;
-        M2       = zeros(1,4);
-        QuadHist(i).edgesArray = zeros(length(QuadHist(i).edges-1),2);
-        QuadHist(i).edgesArray = [QuadHist(i).edges(1:end-1)',QuadHist(i).edges(2:end)'];
-        MA = [repmat(QuadHist(i).angle ,length(QuadHist(i).counts(:,i)),1), QuadHist(i).edgesArray , QuadHist(i).counts(:,i)];
-        M2 = [M2; MA];
-    end
-    M =M2(2:end,:);
-    ind = find(M(:,4) > 0);
-    M = M(ind,:);
+    QuadHist  = QuadHist(2:end,:);
+    ind = find(QuadHist(:,4) > 0);
+    QuadHist =QuadHist(ind,:);
 end
-
+M = QuadHist;
 end
