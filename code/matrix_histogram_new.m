5150ed29ab8a1001c8fdd23a171d02fe3a879037
Leonardo <leonardoguitar2010@gmail.com>
2018-08-29 00:13:53 +0000

    matrix_histogram_new.m created online with Bitbucket

--- code/matrix_histogram_new.m
+++ code/matrix_histogram_new.m
@@ -1,5 +1,16 @@
 function M = matrix_histogram_new(numAngles, samples, option, H_operator,deltaq)
 
+% matrix_histogram_new constructs the matrix of histograms M according to the sample and the measurement operator that you 
+% want to use for the reconstruction of the state: measurement operator representing the measurement in 
+% the center of the box (center) and measuring operator along the length of the box ( integral). 
+% If the option chosen for the use of the measuring operator is "center", M will be an array with columns 
+% (angle, measured in the center of the box, number of counts of the box). If the option chosen for the use 
+% of the measurement operator is "integral", M will be a matrix with columns (angle, left edge of the box, 
+% right edge of the box, number of counts of the box).
+
+% Constructs the histogram matrix using the edges of the boxes whose width is chosen by the Leonhardt method 
+% using the measurement operator along the box.
+
 if nargin == 2
   option = 'bin_leonhardt';
   H_operator = 'integral';
@@ -8,6 +19,9 @@ end
 num_measurements = size(samples, 1);
 
 angles = samples(1:numAngles);
+
+% constructs the histograms using the homodyne measurements of the sample by choosing the method to 
+% calculate the width of the box.
 
 for i=1:numAngles
     QuadHist(i).angle = angles(i);
@@ -32,6 +46,9 @@ for i=1:numAngles
     end
 end
 
+% Constructs M so that the measurement operator represents the measurement that occurs exactly in the center 
+% of the histogram box.
+
 if strcmp(H_operator,'center')
     
     for i = 1:numAngles
@@ -39,7 +56,10 @@ if strcmp(H_operator,'center')
         QuadHist(i).centers = QuadHist(i).edges(1:end-1)+d;
         QuadHist(i).M = [repmat(angles(i),length(QuadHist(i).counts.'),1), QuadHist(i).centers.',QuadHist(i).counts.'];
     end
-    
+   
+% Constructs M so that the measurement operator represents the measure that occurs along the length of the 
+% histogram box.    
+
 elseif strcmp(H_operator,'integral')
     
     for i = 1:numAngles
